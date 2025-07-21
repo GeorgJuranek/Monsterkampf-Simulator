0f330270e2d9b5dc10c3ef3a622ec6b63ae19a13
georgjuranek <Georg_Juranek@hotmail.de>
2025-07-21 16:59:19 +0200

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,360 +1,59 @@
-﻿namespace MonsterkampfSimulator;
-
-class Program
+﻿namespace VektorKlasse
 {
-    private static ConsoleColor copColor = ConsoleColor.DarkBlue;
-    private static ConsoleColor antifaColor = ConsoleColor.DarkRed;
-    private static ConsoleColor hooliganColor = ConsoleColor.DarkYellow;
-
-    private static ConsoleColor errorColor = ConsoleColor.Red;
-    private static ConsoleColor menuColor = ConsoleColor.Gray;
-    private static ConsoleColor gameColor = Console.ForegroundColor;
-
-    private static Fighter firstFighter = null;
-    private static Fighter secondFighter = null;
-
-    private static float newHLT;
-    private static float newATK;
-    private static float newDEF;
-    private static float newSPD;
-
-    private const int maxTurn = 99;
-
-    static void Main(string[] args)
+    internal class Program
     {
-        while (true)
+        static void Main(string[] args)
         {
-            Start();
-            SetUpFighters();
-            GameLoop();
-        }
-    }
+            /*Aufgaben*/
+            Console.WriteLine(" AUFGABEN: ");
 
-    private static void Start()
-    {
-        Console.Clear();
-        ColoredWrite("NEWS: ", errorColor);
-        Console.WriteLine("A large demonstration escalated today. A battle has broken out between different groups.");
-        Console.ReadKey();
-    }
+            VektorDrei myVektor = new VektorDrei();
+            translateVektor(myVektor, "1: Leerer Konstruktor: ");
 
-    private static void SetUpFighters()
-    {
-        Console.ForegroundColor = menuColor;
+            VektorDrei myVektor2 = new VektorDrei(1,2,3);
+            translateVektor(myVektor2, "2: Konstruktor mit Werten: ");
 
-        EType firstFighterType = ChooseFighterType("first");
-        firstFighter = RegistrateFighter(firstFighterType);
-        ColoredWrite($"{firstFighter.Race}", firstFighter.Color);
-        Console.WriteLine(" has arrived!");
-        Console.ReadKey();
+            VektorDrei myVektor3 = new VektorDrei(1, 2, 3);
+            translateVektor(myVektor2, "3: Konstruktor mit Werten: ");
 
-        EType secondFighterType;
-        do
-        {
-            secondFighterType = ChooseFighterType("second");
+            translateVektor(myVektor2 + myVektor3, "Vektoren 2 + 3 addiert: ");
 
-            if (firstFighterType == secondFighterType)
-            {
-                OccuringError("ERROR: Fighters from the same social-class doesnt fight each other.");
-            }
+            translateVektor(myVektor2 - myVektor3, "Vektoren 2 - 3 subtrahiert: ");
 
-        }
-        while (firstFighterType == secondFighterType);
+            translateVektor(myVektor2 * 2, "Vektor 2 mit integer 2 multipliziert: ");
 
-        secondFighter = RegistrateFighter(secondFighterType);
-        ColoredWrite($"{secondFighter.Race}", secondFighter.Color);
-        Console.WriteLine(" has arrived!");
-        Console.ReadKey();
+            Console.WriteLine("Distanz zwischen 3 und 2 (unstatisch): " + myVektor3.Distanz(myVektor2));
+            Console.WriteLine("Distanz zwischen 3 und 2 (statisch): " + VektorDrei.Distanz(myVektor3, myVektor2));
 
-        Console.ForegroundColor = gameColor;
+            Console.WriteLine("Betrag von 2: " + myVektor2.Betrag());
 
-        Console.Clear();
+            Console.WriteLine("Quadrat von 2: " + myVektor2.Quadrat());
 
-        Console.Write("On the one side stood the ");
-        ColoredWrite($"{firstFighter.Race}", firstFighter.Color);
-        Console.Write($", on the other side the ");
-        ColoredWrite($"{secondFighter.Race}", secondFighter.Color);
-        Console.Write(".\n\n");
-        Console.ReadKey();
+            /*Zusatz*/
+            Console.WriteLine("\n ZUSATZ: ");
 
-    }
+            Console.WriteLine("Skalarprodukt von 2 & 3: " + myVektor2.Skalarprodukt(myVektor3));
+            translateVektor(myVektor2.Kreuzprodukt(myVektor3), "Kreuzprodukt von 2 & 3: ");
 
-    private static void GameLoop()
-    {
-        Fighter firstToAttack;
-        Fighter secondToAttack;
+            translateVektor(myVektor2 / 2, "Vektor 2 mit integer 2 dividiert: ");
 
-        if (firstFighter.Spd > secondFighter.Spd)
-        {
-            firstToAttack = firstFighter;
-            secondToAttack = secondFighter;
-        }
-        else
-        {
-            secondToAttack = firstFighter;
-            firstToAttack = secondFighter;
-        }
-        firstToAttack.isFirstToAttack = true;
+            translateVektor(myVektor2*myVektor3, "Vektor 2 mit Vektor 3 multipliziert: ");
 
-        ColoredWrite($"{firstToAttack.Race}", firstToAttack.Color);
-        Console.WriteLine(" has attacked first.");
+            translateVektor(myVektor2.Normalisiert(), "Vektor 2 normalisiert: ");
 
-        Console.ReadKey(true);
+            translateVektor( VektorDrei.Null, "Vektor 'Null': ");
+            translateVektor( VektorDrei.Eins, "Vektor 'Eins': ");
 
-        int turn = 1;
+            translateVektor( VektorDrei.Zufall(5,5,5), "Zufalls Vektor: ");
+            translateVektor(VektorDrei.Zufall(5,5,5), "Zufalls Vektor (2): ");
 
-        while (!firstToAttack.IsDead && !secondToAttack.IsDead)
-        {
-            Console.Write($"{turn}. ");
-            firstToAttack.Attack(secondToAttack);
             Console.ReadKey(true);
-            turn++;
-
-            if (secondToAttack.IsDead)
-                break;
-
-            Console.Write($"{turn}. ");
-            secondToAttack.Attack(firstToAttack);
-            Console.ReadKey(true);
-            turn++;
-
-            if (hasOversteppedMaximalTurns(turn))
-            {
-                break;
-            }
         }
 
-        Console.Clear();
 
-        if (firstToAttack.IsDead)
+        static void translateVektor(VektorDrei vektor, string text)
         {
-            secondToAttack.PrintWinningText();
+            Console.WriteLine(text + vektor.x + "/" + vektor.y + "/" + vektor.z);
         }
-        else if (secondToAttack.IsDead)
-        {
-            firstToAttack.PrintWinningText();
-        }
-        else
-        {
-            Console.WriteLine("The situation ended in a draw and could only be ended by the deployment of the military.");
-        }
-
-        Console.Write("\n");
-
-        if (turn>1)
-        {
-            Console.Write($"\n(Ended after {turn} moves)\n");
-        }
-        else
-        {
-            Console.Write($"\n(Ended after a single move)\n");
-        }
-        Console.ReadKey(true);
-    }
-
-    private static bool hasOversteppedMaximalTurns(int turn)
-    {
-        return (turn > maxTurn);
-
-    }
-
-    private static EType ChooseFighterType(string numeration)
-    {
-        int fighterIndex;
-        bool isCorrectInput = false;
-        bool wasIncorrect = false;
-
-        Console.Clear();
-        Console.Write($"Choose the {numeration} Group: \n");
-        Console.Write("[1]-");
-        ColoredWrite("COP ", copColor);
-        Console.Write("[2]-");
-        ColoredWrite("ANTIFA ", antifaColor);
-        Console.Write("[3]-");
-        ColoredWrite("HOOLIGAN ", hooliganColor);
-        Console.Write("\n");
-
-        do
-        {
-            if (wasIncorrect)
-            {
-                OccuringError("ERROR! Input was not accepted, try something else.");
-                wasIncorrect = false;
-            }
-
-            isCorrectInput = int.TryParse(Console.ReadLine(), out fighterIndex);
-
-            wasIncorrect = true;
-        }
-        while (!isCorrectInput || fighterIndex > 3 || fighterIndex <= 0);
-
-        return (EType)fighterIndex;
-    }
-
-    private static Fighter RegistrateFighter(EType fighterType)
-    {
-        ConsoleColor fighterColor = AssignColor((int)fighterType);
-
-        Console.Write("You have chosen the ");
-        ColoredWrite($"{fighterType}", fighterColor);
-        Console.Write(".\n\n");
-
-        float abilityPoints = 10;
-
-        bool hasPassedCustomization = false;
-
-        do
-        {
-            abilityPoints = 10;
-            ColoredWrite($"You have {abilityPoints} Abilitypoints to customize the {fighterType}\n" +
-            $" <HEALTH> <ATTACK> <DEFENSE> <SPEED>\n" +
-            $"(Every must contain at least a value of 1)\n\n", ConsoleColor.Yellow);
-
-            Console.Write("Choose HEALTH:");
-            newHLT = RegistrateCustomStat(10);
-            abilityPoints -= newHLT;
-            ColoredWrite($"you have {abilityPoints} AP left.\n\n", ConsoleColor.Yellow);
-
-            if (abilityPoints > 0)
-            {
-                Console.Write("Choose ATTACK:");
-                newATK = RegistrateCustomStat(5);
-                abilityPoints -= newATK;
-                ColoredWrite($"you have {abilityPoints} AP left.\n\n", ConsoleColor.Yellow);
-            }
-
-            if (abilityPoints > 0)
-            {
-                Console.Write("Choose DEFENSE:");
-                newDEF = RegistrateCustomStat(5);
-                abilityPoints -= newDEF;
-                ColoredWrite($"you have {abilityPoints} AP left.\n\n", ConsoleColor.Yellow);
-            }
-
-            if (abilityPoints > 0)
-            {
-                Console.Write("Choose SPEED:");
-                newSPD = RegistrateCustomStat(5);
-                abilityPoints -= newSPD;
-                ColoredWrite($"you have {abilityPoints} AP left.\n\n", ConsoleColor.Yellow);
-
-                if (abilityPoints >= 0)
-                {
-                    hasPassedCustomization = true;
-                }
-            }
-
-            if (!hasPassedCustomization)
-            {
-                Console.Clear();
-                Console.Write("\n");
-                OccuringError("ERROR: Outside of Ability Points Maximum!\n\n");
-            }
-
-        } while (!hasPassedCustomization);
-
-        Console.Write("\nTYPE:");
-        ColoredWrite($"{fighterType}", fighterColor);
-        Console.Write("\nHLT:<");
-        ColoredWrite($"{newHLT}", fighterColor);
-        Console.Write("> ATK:<");
-        ColoredWrite($"{newATK}", fighterColor);
-        Console.Write("> DEF:<");
-        ColoredWrite($"{newDEF}", fighterColor);
-        Console.Write("> SPD:<");
-        ColoredWrite($"{newSPD}", fighterColor);
-        Console.Write(">\n\n");
-
-        return CreateFighter((int)fighterType, fighterColor, newHLT, newATK, newDEF, newSPD);
-    }
-
-    private static float RegistrateCustomStat(float maximum)
-    {
-        Console.Write($"\n(the stat must be a number between 1 - {maximum})\n");
-
-        float newStat = 0;//default
-        bool isCorrectNewState = false;
-        bool isWrongValue = false;
-
-        do
-        {
-            if (isWrongValue)
-            {
-                OccuringError("ERROR: type in a number inside the given values!");
-                isWrongValue = false;
-            }
-
-            isCorrectNewState = float.TryParse(Console.ReadLine(), out newStat);
-            isWrongValue = true;
-
-        } while (!isCorrectNewState || newStat < 1 || newStat > maximum);
-
-        return newStat;
-    }
-
-    private static void DeleteCurrentLine(int line)
-    {
-        int bufferWidth = Console.BufferWidth;
-
-        Console.SetCursorPosition(0, line);
-        for (int i = 0; i < bufferWidth; i++)
-        {
-            Console.Write(" ");
-        }
-        Console.SetCursorPosition(0, line);
-    }
-
-    private static void OccuringError(string errorMessage = "")
-    {
-        int startLine = Console.GetCursorPosition().Top;
-        DeleteCurrentLine(startLine);
-
-        Console.SetCursorPosition(Console.GetCursorPosition().Left, startLine - 1);
-        DeleteCurrentLine(startLine-1);
-
-        ColoredWrite(errorMessage, errorColor);
-
-        Console.ReadKey();
-        DeleteCurrentLine(startLine-1);
-    }
-
-
-    private static Fighter CreateFighter(int newFighterIndex, ConsoleColor newColor, float newHLT, float newATK, float newDEF, float newSPD)
-    {
-        switch(newFighterIndex)
-        {
-            case (int)EType.COP:
-                return new Cop(newHLT, newATK, newDEF, newSPD, newColor);
-            case (int)EType.ANTIFA:
-                return new Antifa(newHLT, newATK, newDEF, newSPD, newColor);
-            case (int)EType.HOOLIGAN:
-                return new Hooligan(newHLT, newATK, newDEF, newSPD, newColor);
-            default:
-                return new Cop(newHLT, newATK, newDEF, newSPD, ConsoleColor.Magenta);//ERROR
-        }
-    }
-
-    private static ConsoleColor AssignColor(int newFighterIndex)
-    {
-        switch (newFighterIndex)
-        {
-            case (int)EType.COP:
-                return copColor;
-            case (int)EType.ANTIFA:
-                return antifaColor;
-            case (int)EType.HOOLIGAN:
-                return hooliganColor;
-            default:
-                return ConsoleColor.Magenta;//ERROR
-        }
-    }
-
-    private static void ColoredWrite(string content, ConsoleColor color)
-    {
-        ConsoleColor currentForegroundColor = Console.ForegroundColor;
-        Console.ForegroundColor = color;
-        Console.Write(content);
-        Console.ForegroundColor = currentForegroundColor;
     }
 }
-
---
+++ VektorDrei.cs
@@ -0,0 +1,158 @@
+﻿public class VektorDrei
+{
+    #region Aufgaben
+    /*Drei float Attribute/Felder: x, y, z*/
+    private float X, Y, Z;
+
+    public float x { get => X; set => X=value; }
+    public float y { get => Y; set => Y=value; }
+    public float z { get => Z; set => Z=value; }
+
+    /*Standardkonstruktor in dem x,y,z auf 0 gesetzt werden */
+    public VektorDrei()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    /*Einen Konstruktor in dem x,y,z mit Parametern initialisiert werden*/
+    public VektorDrei(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    /*+ Operator für die Addition mit einem anderen Vektor*/
+    public static VektorDrei operator +(VektorDrei first, VektorDrei second)
+    {
+        float resultX = first.x + second.x;
+        float resultY = first.y + second.y;
+        float resultZ = first.z + second.z;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*- Operator für die Substraktion mit einem anderen Vektor*/
+    public static VektorDrei operator -(VektorDrei first, VektorDrei second)
+    {
+        float resultX = first.x - second.x;
+        float resultY = first.y - second.y;
+        float resultZ = first.z - second.z;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /** Operator für die Multiplikation mit einem Skalar (Zahl)*/
+    public static VektorDrei operator *(VektorDrei vektor, int skalar)
+    {
+        float resultX = vektor.x * skalar;
+        float resultY = vektor.y * skalar;
+        float resultZ = vektor.z * skalar;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*Methode die die Distanz zwischen zwei Vektoren/Punkten berechnet und als float zurückgeben. Implementiere diese Methode in einer statischen und nicht-statischen Version.*/
+    public float Distanz(VektorDrei other)
+    {
+        return Distanz(this,other);
+    }
+    public static float Distanz(VektorDrei first, VektorDrei second)
+    {
+        VektorDrei result = second - first;
+        return result.Betrag();
+    }
+
+    /*Methode die die Länge eines Vektors berechnet und als float ausgibt.*/
+    public float Betrag()
+    {
+        return (float)Math.Sqrt(Quadrat());
+    }
+
+    /*Methode die die Quadratlänge eines Vektors berechnet und als float ausgibt.*/
+    public float Quadrat()
+    {
+        float resultX = x * x;
+        float resultY = y * y;
+        float resultZ = z * z;
+        float resultXYZ = resultX + resultY + resultZ;
+        return resultXYZ;
+    }
+
+    #endregion
+
+    #region Zusatz
+
+    /*Punkt|Skalar Produkt*/
+    public float Skalarprodukt(VektorDrei other)
+    {
+        float resultX = x * other.x;
+        float resultY = y * other.y;
+        float resultZ = z * other.z;
+        float resultXYZ = resultX + resultY + resultZ;
+        return resultXYZ;
+    }
+    public float Punktprodukt(VektorDrei other)
+    {
+        return Skalarprodukt(other);
+    }
+
+    /*Kreuz Produkt*/
+    public VektorDrei Kreuzprodukt(VektorDrei other)
+    {
+        float resultX = y * other.z - z * other.y;
+        float resultY = z * other.x - x * other.z;
+        float resultZ = x * other.y - y * other.x;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*Division mit Skalar*/
+    public static VektorDrei operator /(VektorDrei vektor, int skalar)
+    {
+        float resultX = vektor.x / skalar;
+        float resultY = vektor.y / skalar;
+        float resultZ = vektor.z / skalar;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*Vektor mit Vektor multiplikation*/
+    public static VektorDrei operator *(VektorDrei first, VektorDrei second)
+    {
+        float resultX = first.x * second.x;
+        float resultY = first.y * second.y;
+        float resultZ = first.z * second.z;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*Normalisiert*/
+    public VektorDrei Normalisiert()
+    {
+        float normal = Betrag();
+        float resultX = x/normal;
+        float resultY = y/normal;
+        float resultZ = z/normal;
+        return new VektorDrei(resultX, resultY, resultZ);
+    }
+
+    /*Voreinstellungen*/
+    public static VektorDrei Eins => new VektorDrei(1,1,1);
+    public static VektorDrei Null => new VektorDrei(0,0,0);
+    public static VektorDrei Vorwaerts => new VektorDrei(0,0,1);
+    public static VektorDrei Rueckwaerts => new VektorDrei(0,0,-1);
+    public static VektorDrei Links => new VektorDrei(-1,0,0);
+    public static VektorDrei Rechts => new VektorDrei(1,0,0);
+    public static VektorDrei Oben => new VektorDrei(0,1,0);
+    public static VektorDrei Unten => new VektorDrei(0,-1,0);
+
+    /*Zufall*/
+    public static VektorDrei Zufall(int ranX, int ranY, int ranZ)
+    {
+        Random random = new Random();
+        ranX = random.Next(0, ranX+1);
+        ranY = random.Next(0, ranY+1);
+        ranZ = random.Next(0, ranZ+1);
+        return new VektorDrei(ranX, ranY, ranZ);
+    }
+
+    #endregion
+}
+
